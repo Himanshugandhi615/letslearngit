f15fcc110f2970710497a2bc90f5984e831f6549
Himanshu gandhi <gandhihimanshu58@gmail.com>
2021-07-16 17:19:47 +0530

    initial sum.java

---
+++ sum.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class sum
+{
+    public static void main(String []args)
+    {
+         Scanner obj=new Scanner(System.in);
+         System.out.print("Enter first num:");
+         int a=obj.nextInt();
+         System.out.print("Enter second num:");
+         int b=obj.nextInt();
+         int c=a+b;
+         System.out.print("Sum is: "+c);
+    }
+}
